4ef1fdda43e678b783c2c67419627760f1a277fe
Li Jianshu <lijianshu2015@gmail.com>
2017-03-10 21:31:07 +0800

    Update setGPU.py

--- setGPU.py
+++ setGPU.py
@@ -3,10 +3,8 @@ import gpustat
 
 stats = gpustat.GPUStatCollection.new_query()
 ids = map(lambda gpu: int(gpu.entry['index']), stats)
-ratios = map(
-    lambda gpu: float(gpu.entry['memory.used'])/float(gpu.entry['memory.total'])
-    stats)
-bestGPU = min(zip(ids, ratios), key=lambda x: x[1])[0]
+available_mems = map(lambda gpu: float(gpu.entry['memory.total'])-float(gpu.entry['memory.used']), stats)
+bestGPU = max(zip(ids, available_mems), key=lambda x: x[1])[0]
 
 print("setGPU: Setting GPU to: {}".format(bestGPU))
 os.environ['CUDA_DEVICE_ORDER'] = 'PCI_BUS_ID'
